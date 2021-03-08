cedce0417881fec5aff72fe91e6d0eb4c6cfda14
Giacomo <giacomo.1936@gmail.com>
2021-03-08 14:07:58 +0100

    modifica margini header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,42 @@
 <header :class="isActive ? 'active' : ''">
-
-    <div id="wp-header" class="wp-header">
-
-        <div id="menu-fixed">
-
-            <div class="header-navbar">
+    <div class="container">
+        <div class="header-navbar">
+            <div class="header-logo">
                 <a href="{{ route('welcome') }}">
                     <div class="logo">
-                        <img src="{{ asset('images/logo-studio.png') }}" alt="logo-studio-legale">
+                            <img src="{{ asset('images/logo-studio.png') }}" alt="logo-studio-legale">
                     </div>
                 </a>
-                <div class="header-right">
-                    <div class="toggle-menu"  @click="toggleMenu()">
-                        <i class="fas fa-bars"></i>
-                    </div>
-                </div>
-
+            </div>
+            <div class="header-menu">
+                <ul>
+                    <li>
+                        <a href="#">Professionisti</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Contatti</a>
+                    </li>
+                    <li>
+                        <a href="#">Risorse</a>
+                    </li>
+                    <li>
+                        <a href="#">Competenze</a>
+                    </li>
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                </ul>
+                {{-- <div class="toggle-menu"  @click="toggleMenu()">
+                    <i class="fas fa-bars"></i>
+                </div> --}}
             </div>
 
-
         </div>
+    </div>
 </header>
